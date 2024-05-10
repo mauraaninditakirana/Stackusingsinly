161e278d1624b6945b16a6406ccd2d9ed01f2ade
mauraaninditakirana <kiranamaura351@gmail.com>
2024-05-10 14:16:59 +0700

    membuat case 2 dalam switch case untuk keluarkan elemen teratas dari tumpukan

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -72,7 +72,15 @@ int main(){
             cin >> value;
             stack.push(value);
             break;
-
+        case 2:
+            if (!stack.isEmpty()) {
+                stack.pop();
+            }
+            else {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+        
         }
     }
 }
